d5590f2dbce2819cde3310bf99fc5373dc96f597
Anistina Dsouza <dsouzaanistina@gmail.com>
2026-04-08 12:42:36 +0530

    Add ProductModel, abstract Product interface, and SimpleProduct implementation with stock validation

---
+++ inventory/components/product.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+#template, (interface Pattern) create base structure for all products 
+class Product(ABC):
+     #decorator - forces the subclasses to implement following methods
+    @abstractmethod
+    def getStock(self):
+        pass   #do nothing, just a placeholder
+
+    @abstractmethod
+    def reduceStock(self, qty):
+        pass
+
+    @abstractmethod
+    def getPrice(self):
+        pass
