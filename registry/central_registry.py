5d968db16d931db4363bb32838adce78df0468fc
Maryam Shaikh <mshaikh191103@gmail.com>
2026-04-22 12:01:54 +0530

    feature: central registry

---
+++ registry/central_registry.py
@@ -0,0 +1,29 @@
+class CentralRegistry:
+    """
+    Singleton Pattern
+    Maintains a single global instance for system configuration.
+    """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._config = {}
+            cls._instance._kiosks = {}
+        return cls._instance
+
+    def registerKiosk(self, kiosk_id, kiosk_ref):
+        self._kiosks[kiosk_id] = kiosk_ref
+        print(f"[REGISTRY] Kiosk registered: {kiosk_id}")
+
+    def getKiosk(self, kiosk_id):
+        return self._kiosks.get(kiosk_id)
+
+    def listKiosks(self):
+        return list(self._kiosks.keys())
+
+    def setConfig(self, key, value):
+        self._config[key] = value
+
+    def getConfig(self, key):
+        return self._config.get(key)
